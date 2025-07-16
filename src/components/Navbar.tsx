b9b57a376a4a5f9edf71ffc24510816a1fea5550
aramind <88814622+aramind@users.noreply.github.com>
2025-07-16 22:51:56 +0800

    feat: finalizing the dropdown in the profile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,15 @@
 import Link from "next/link";
 import React from "react";
-import { Moon } from "lucide-react";
+import { LogOut, Moon, Settings, User } from "lucide-react";
 import AvatarReplacement from "./AvatarReplacement";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export default function Navbar() {
   return (
@@ -12,11 +20,33 @@ export default function Navbar() {
       <div className="flex items-center gap-4">
         <Link href="/">Dashboard</Link>
         <Moon />
-        <AvatarReplacement
-          src="https://github.com/shadcn.png"
-          alt="User Avatar"
-          size={48}
-        />
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <button className="cursor-pointer">
+              <AvatarReplacement
+                src="https://github.com/shadcn.png"
+                alt="User Avatar"
+                size={48}
+              />
+            </button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent sideOffset={10}>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem className="cursor-pointer">
+              <User className="h-[1.2rem] w-[1.2rem] mr-2" />
+              Profile
+            </DropdownMenuItem>
+            <DropdownMenuItem className="cursor-pointer">
+              <Settings className="h-[1.2rem] w-[1.2rem] mr-2" />
+              Settings
+            </DropdownMenuItem>
+            <DropdownMenuItem className="cursor-pointer" variant="destructive">
+              <LogOut className="h-[1.2rem] w-[1.2rem] mr-2" />
+              Logout
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </nav>
   );
