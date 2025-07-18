0be83c0f52fe124c1ab80e1ccfac2f9ac516e757
aramind <88814622+aramind@users.noreply.github.com>
2025-07-19 07:18:52 +0800

    feat: adding pie chart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import AppAreaChart from "@/components/AppAreaChart";
 import AppBarChart from "@/components/AppBarChart";
+import AppPieChart from "@/components/AppPieChart";
 
 export default function Home() {
   return (
@@ -8,7 +9,9 @@ export default function Home() {
         <AppBarChart />
       </div>
       <div className="bg-primary-foreground p-4 rounded-lg">2</div>
-      <div className="bg-primary-foreground p-4 rounded-lg">3</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">
+        <AppPieChart />
+      </div>
       <div className="bg-primary-foreground p-4 rounded-lg">4</div>
       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2">
         <AppAreaChart />
