148991336e33f65d9c675809d3770b1813145011
aramind <88814622+aramind@users.noreply.github.com>
2025-07-19 16:12:12 +0800

    feat: adding layout of single user page

---
+++ src/app/users/[username]/page.tsx
@@ -0,0 +1,71 @@
+import CardList from "@/components/CardList";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import React from "react";
+
+export default function SingleUserPage() {
+  return (
+    <div>
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/">Dashboard</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/users">Users</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Dashboard</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+      {/* CONTAINER */}
+      <div className="mt-4 flex flex-col xl:flex-row gap-8">
+        {/* LEFT */}
+        <div className="w-full xl-w-1/3 space-y-6">
+          {/* USER BADGES */}
+          <div className="bg-primary-foreground p-4 rounded-lg">
+            <h1 className="text-xl font-semibold">User Badges</h1>
+            <div className="flex gap-4 mt-4"></div>
+          </div>
+          {/* INFORMATION CENTER */}
+          <div className="bg-primary-foreground p-4 rounded-lg">
+            <h1 className="text-xl font-semibold">Info</h1>
+            <div className="flex gap-4 mt-4"></div>
+          </div>
+          {/* CARD LIST CONTAINER */}
+          <div className="bg-primary-foreground p-4 rounded-lg">
+            <CardList title="Recent Transactions" />
+          </div>
+        </div>
+        {/* RIGHT */}
+        <div className="w-full xl:w-2/3 space-y-6">
+          {/* USER CARD CONTAINER */}
+          <div className="bg-primary-foreground p-4 rounded-lg space-y-2">
+            <div className="flex items-center gap-2">
+              <Avatar className="size-12">
+                <AvatarImage src="https://avatars.githubusercontent.com/u/1486366" />
+                <AvatarFallback>JD</AvatarFallback>
+              </Avatar>
+              <h1 className="text-xl font-semibold">John Doe</h1>
+            </div>
+            <p className="text-sm text-muted-foreground">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Provident, obcaecati sapiente! Beatae maiores eius corporis nisi
+              quam quae, sunt nostrum!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
