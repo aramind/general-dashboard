bcfb537c73de1ac119815892aa60470e716daff3
aramind <88814622+aramind@users.noreply.github.com>
2025-07-19 15:45:43 +0800

    feat: adding calendar

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,9 +1,14 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { ScrollArea } from "./ui/scroll-area";
 import { Card } from "./ui/card";
 import { Label } from "recharts";
 import { Checkbox } from "./ui/checkbox";
 import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+import { Calendar } from "@/components/ui/calendar";
+import { Button } from "./ui/button";
+import { format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
 
 const ListItem = ({ index }: { index: number }) => {
   return (
@@ -23,11 +28,30 @@ const ListItem = ({ index }: { index: number }) => {
 };
 
 export default function TodoList() {
+  const [date, setDate] = useState<Date | undefined>(new Date());
+  const [open, setOpen] = useState(false);
+
   return (
     <div>
-      <Popover>
-        <PopoverTrigger>Open</PopoverTrigger>
-        <PopoverContent>Place content for the popover here.</PopoverContent>
+      <h1 className="text-lg font-medium mb-6">Todo List</h1>
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button className="w-full">
+            <CalendarIcon />
+            {date ? format(date, "PPP") : <span>Pick a date</span>}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="p-0 w-auto">
+          <Calendar
+            mode="single"
+            selected={date}
+            onSelect={(date) => {
+              setDate(date);
+              setOpen(false);
+            }}
+            className="rounded-lg border"
+          />
+        </PopoverContent>
       </Popover>
       {/* LIST */}
       <ScrollArea className="max-h-[600px] mt-4  overflow-y-auto">
