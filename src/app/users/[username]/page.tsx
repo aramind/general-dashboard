29fb94cabf9d6484ee3697a0a9b756fdf2793770
aramind <88814622+aramind@users.noreply.github.com>
2025-07-20 01:17:58 +0800

    feat: adding line chart in the user page

--- src/app/users/[username]/page.tsx
+++ src/app/users/[username]/page.tsx
@@ -1,3 +1,4 @@
+import AppLineChart from "@/components/AppLineChart";
 import CardList from "@/components/CardList";
 import EditUser from "@/components/EditUser";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
@@ -172,6 +173,10 @@ export default function SingleUserPage() {
               Provident, obcaecati sapiente! Beatae maiores eius corporis nisi
               quam quae, sunt nostrum!
             </p>
+          </div>
+          <div className="bg-primary-foreground p-4 rounded-lg">
+            <h1 className="text-xl font-semibold">User Activity</h1>
+            <AppLineChart />
           </div>
         </div>
       </div>
