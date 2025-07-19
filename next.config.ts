b6f5dc9a05329a06b74c6d16b6da690d9036239b
aramind <88814622+aramind@users.noreply.github.com>
2025-07-19 14:36:54 +0800

    feat: adding card list

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["github.com"], // ✅ add your image host domain here
+    domains: ["github.com", "images.pexels.com"], // ✅ add your image host domain here
   },
 };
 
